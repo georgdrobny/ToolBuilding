ccb7246641b21ab540f7a2bd07f0ec32ad69af5b
Georg Drobny <81366735+georgdrobny@users.noreply.github.com>
2022-11-22 16:54:45 +0100

    Update Demo1.ps1

--- Demo1.ps1
+++ Demo1.ps1
@@ -2,6 +2,8 @@ Function Remote-Changes
 {
   [CmdLetBinding()]
   Param()
+  # Added code on the remote (GitHub)
+  Write-Host -ForgroundColor Green "Hello from Remote Server"
 }
 
 Function Local-Changes
