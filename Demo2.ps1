01c61d67e2463a68287821346c7a2bd52e877489
Georg Drobny <81366735+georgdrobny@users.noreply.github.com>
2022-11-23 08:44:49 +0100

    Update Demo2.ps1

--- Demo2.ps1
+++ Demo2.ps1
@@ -1,1 +1,15 @@
-Get-Service BITS
+Function Remote-Changes 
+{
+  [CmdLetBinding()]
+  Param()
+  # Add some code on the remote (GitHub)
+ 
+}
+
+Function Local-Changes
+{
+  [CmdLetBinding()]
+  Param()
+  # Added some code on the local machine
+ 
+}
